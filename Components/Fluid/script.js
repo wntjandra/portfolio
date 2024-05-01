467596d9e2960bff6ce61d860cea408df6b9f3c4
William Tjandra <williamtj2001@gmail.com>
2024-04-30 18:45:44 -0700

    Added text

--- Components/Fluid/script.js
+++ Components/Fluid/script.js
@@ -47,7 +47,7 @@ let config = {
     PAUSED: false,
     BACK_COLOR: { r: 0, g: 0, b: 0 },
     TRANSPARENT: false,
-    BLOOM: true,
+    BLOOM: false,
     BLOOM_ITERATIONS: 8,
     BLOOM_RESOLUTION: 256,
     BLOOM_INTENSITY: 0.8,
